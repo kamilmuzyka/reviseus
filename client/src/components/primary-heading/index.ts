f111a98ded562c73ee1f2a544d2dc2d71b7524ba
kamilmuzyka <kamil.muzyka@gmail.com>
2021-04-08 00:53:29 +0200

    Protect custom elements from being re-defined

--- client/src/components/primary-heading/index.ts
+++ client/src/components/primary-heading/index.ts
@@ -1,20 +1,22 @@
 const PrimaryHeading = (): void => {
-    customElements.define(
-        'primary-heading',
-        class extends HTMLElement {
-            constructor() {
-                super();
-                const template = document.createElement('template');
-                template.innerHTML = `
-                  <h2>
-                    <slot></slot>
-                  </h2>
-                `;
-                const shadowRoot = this.attachShadow({ mode: 'closed' });
-                shadowRoot.appendChild(template.content.cloneNode(true));
+    if (!customElements.get('primary-heading')) {
+        customElements.define(
+            'primary-heading',
+            class extends HTMLElement {
+                constructor() {
+                    super();
+                    const template = document.createElement('template');
+                    template.innerHTML = `
+                      <h2>
+                        <slot></slot>
+                      </h2>
+                    `;
+                    const shadowRoot = this.attachShadow({ mode: 'open' });
+                    shadowRoot.appendChild(template.content.cloneNode(true));
+                }
             }
-        }
-    );
+        );
+    }
 };
 
 export default PrimaryHeading;
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,22 +1,24 @@
 import PrimaryHeading from './components/primary-heading/index';
 
 const App = (): void => {
-    customElements.define(
-        'the-app',
-        class extends HTMLElement {
-            constructor() {
-                super();
-                const template = document.createElement('template');
-                template.innerHTML = `
-                  <div>
-                    <primary-heading>Revise.us</primary-heading>
-                  </div>
-                `;
-                const shadowRoot = this.attachShadow({ mode: 'closed' });
-                shadowRoot.appendChild(template.content.cloneNode(true));
+    if (!customElements.get('the-app')) {
+        customElements.define(
+            'the-app',
+            class extends HTMLElement {
+                constructor() {
+                    super();
+                    const template = document.createElement('template');
+                    template.innerHTML = `
+                      <div>
+                        <primary-heading>Revise.us</primary-heading>
+                      </div>
+                    `;
+                    const shadowRoot = this.attachShadow({ mode: 'open' });
+                    shadowRoot.appendChild(template.content.cloneNode(true));
+                }
             }
-        }
-    );
+        );
+    }
 };
 
 PrimaryHeading();
