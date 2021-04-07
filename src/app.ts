be77d8e27f37019fc91760ac5563fd4f23eb5faa
kamilmuzyka <kamil.muzyka@gmail.com>
2021-04-07 20:31:45 +0200

    Serve static files

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
+import path from 'path';
 import express from 'express';
 
+/** Define local constants */
+const PORT = process.env.PORT || 8080;
+const DIRNAME = process.env.PWD || '';
+
+/** Create Express application */
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendStatus(200);
+/** Set up middleware */
+app.use(express.json());
+
+/** Serve static files */
+app.use(express.static(path.join(DIRNAME, 'client', 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(DIRNAME, 'client', 'dist', 'index.html'));
 });
 
-app.listen(8080);
+/** Start the application */
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
