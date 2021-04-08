8f03cde0ee00c3f1ee2ea046f43b28bef7a05869
kamilmuzyka <kamil.muzyka@gmail.com>
2021-04-09 00:58:56 +0200

    Wrap code comments

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,11 @@
 /** @module Component/Application */
 import PrimaryHeading from './components/primary-heading/index';
 
-/** Top-level application component that makes use of all existing components. It's installed in index.html as an entry point to JavaScript application. All components within the application are functions that define custom elements. A component function must execute before using a component. It's to ensure that a component is defined as a custom element. */
+/** Top-level application component that makes use of all existing components.
+ * It's installed in index.html as an entry point to JavaScript application. All
+ * components within the application are functions that define custom elements.
+ * A function must be executed before using a component. It's to ensure that a
+ * component is defined as a custom element. */
 const App = (): void => {
     if (!customElements.get('the-app')) {
         customElements.define(
