e9df507b0323b5882a0ff1c0cbc80b552a1d0877
kamilmuzyka <kamil.muzyka@gmail.com>
2021-04-14 04:13:06 +0200

    Create user model

---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/sequelize-config.js';
+
+const User = sequelize.define(
+    'User',
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        profilePhoto: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        googleId: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    { timestamps: false }
+);
+
+export default User;
