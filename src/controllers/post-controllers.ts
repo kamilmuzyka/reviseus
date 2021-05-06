12700d84c2a0fae753acd45e433fe43c182b12ea
kamilmuzyka <kamil.muzyka@gmail.com>
2021-05-06 14:32:42 +0200

    Expand the createPost controller

--- src/controllers/post-controllers.ts
+++ src/controllers/post-controllers.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { validateNewPost } from '../lib/validate.js';
 import Post from '../models/post-model.js';
 import User from '../models/user-model.js';
+import Tag from '../models/tag-model.js';
 
 export const createPost = async (
     req: Request,
@@ -10,16 +11,52 @@ export const createPost = async (
 ): Promise<void> => {
     try {
         const { userId } = req.user;
-        const { title, content } = validateNewPost(req.body);
-        const user = await User.findOne({ where: { id: userId } });
+        const { title, content, tags } = validateNewPost(req.body);
+
+        /** Find the current user. */
+        const user = await User.findOne({
+            where: { id: userId },
+            include: [Post],
+        });
         if (!user) {
             throw Error(
                 'Could not find a user with the corresponding user ID.'
             );
         }
-        const post = await Post.create({ title, content });
-        await user.$set('posts', [post]);
-        res.sendStatus(200);
+
+        /** Create a Post instance and associate it with the current user. */
+        const newPost = await Post.create({ title, content });
+        await user.$set('posts', [...user.posts, newPost]);
+
+        /** Find or create any attached tags and associate them with the new
+         * post. */
+        if (tags && tags.length) {
+            const postTags = await Promise.all(
+                tags.map(async (tag) => {
+                    const postTag = await Tag.findOrCreate({
+                        where: {
+                            name: tag,
+                        },
+                    });
+                    return postTag[0];
+                })
+            );
+            await newPost.$set('tags', postTags);
+        }
+
+        //** To do: <Check for file> */
+
+        //** To do: <Check for group> */
+
+        /** Retrieve the mutated new post from the database and send it to the
+         * client. */
+        const createdPost = await Post.findOne({
+            where: {
+                id: newPost.id,
+            },
+            include: [User, Tag],
+        });
+        res.json(createdPost);
         return;
     } catch (error) {
         res.status(400).json(error.message);
